51e54597c1236b27fc132b67880171a941a229f6
daneliell <dyue0003@student.monash.edu>
2022-02-17 22:06:45 +0800

    added stars

--- main.js
+++ main.js
@@ -57,8 +57,17 @@ function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({color:0xffffff});
   const star = new THREE.Mesh(geometry, material);
+
+  // generates number between -100 to 100
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x,y,z);
+  scene.add(star);
 }
 
+for (let i=0; i<200; i++) {
+  addStar();
+}
 
 
 // need to render again to see object
